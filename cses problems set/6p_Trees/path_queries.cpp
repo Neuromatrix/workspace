a190a250d76c6d203a4463450c76e9a0a6d156e9
Neuro <neuronsmatrix@gmail.com>
2021-11-26 18:03:53 +0300

    cses sol

---
+++ cses problems set/6p_Trees/path_queries.cpp
@@ -0,0 +1,159 @@
+#include <iostream>
+#include <iomanip>
+#include <ostream>
+#include <fstream>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <bitset>
+#include <vector>
+#include <string>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <array>
+#include <algorithm>
+#include <functional>
+#include <cmath>
+#include <time.h>
+#include <random>
+#include <chrono>
+#include <cassert>
+#include <cstring>
+#include <climits>
+#include <ext/rope>
+#include <ext/pb_ds/detail/standard_policies.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+using ll = long long;   
+using ull = unsigned long long;
+using ld = long double;
+template <typename T> using ordered_set = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+constexpr int INF = INT_MAX-1;
+constexpr ll LINF = LLONG_MAX-1;
+constexpr ll MOD = 1000000007;
+constexpr char nl = '\n';
+constexpr long double eps = 1e-9;
+#define pb push_back
+#define F first
+#define S second
+#define pii pair<int,int>
+#define tiii tuple<int,int,int>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vii vector<pii>
+#define vc vector
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define incr(i,a,b) for (ll i=a; i<b; ++i)
+#define decr(i,a,b) for (ll i=a; i>b; --i)
+#define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define setpr(x) cout<<setprecision(x)<<fixed
+#define sz(x) (int)x.size()
+#define seea(a,x,y) for(int i=x;i<y;i++){cin>>a[i];}
+#define seev(v,n) for(int i=0;i<n;i++){int x; cin>>x; v.push_back(x);}
+#define sees(s,n) for(int i=0;i<n;i++){int x; cin>>x; s.insert(x);}
+#define fca(a,s) for(const auto & a: s)
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+template <typename T> inline T abs(T &x) {return(x<0 ? -x : x);}
+template <typename T> ostream& operator<<(ostream &out, const vector<T> &v) {for (auto &it : v) {out << it << " ";}return out;}
+template <typename T1, typename T2> ostream& operator<<(ostream &out, const pair<T1, T2> &v) {out << v.fi << " " << v.se;return out;}
+template <typename T> inline T pw(T x) {return x*x;}
+template <typename T> inline T pw2(T x){return 1LL<<x;}
+template <typename T> inline T gcd(T a, T b){
+    if (b==0) return a; 
+    else return gcd(b,a%b);
+}
+template <typename T> inline T bin_cof(T n, T k, ull mod = LLONG_MAX){
+    if (k==n || k==0) return 1LL; 
+    else return bin_cof(n-1,k-1,mod)%mod+bin_cof(n-1,k,mod)%mod;
+} 
+template <typename T> inline T factorial(T n){
+    if(n==1) return 1LL; 
+    else return n*factorial(n-1);
+}
+template <typename T> 
+T pow(T a, T poww,long long mod = LLONG_MAX){
+	if (a == 1 || poww == 0) return 1LL;
+    else if (poww == 1) return a%=mod; 
+    else {
+		T  part = pow(a, poww >> 1,mod)%mod;
+		if (poww & 1) return (((part * part)%mod) * a)%mod; 
+        else  return (part * part)%mod;
+	}
+}
+inline void prepare(){
+    freopen("C:\\Users\\grivi\\vscodes\\.vscode\\input.txt", "r", stdin);
+    freopen("C:\\Users\\grivi\\vscodes\\.vscode\\output.txt", "w", stdout);
+}
+struct FT {
+	vector<ll> s;
+	FT(int n) : s(n) {}
+	void update(int pos, ll dif) { // a[pos] += dif
+		for (; pos < sz(s); pos |= pos + 1) s[pos] += dif;
+	}
+	ll query(int pos) { // sum of values in [0, pos)
+		ll res = 0;
+		for (; pos > 0; pos &= pos - 1) res += s[pos-1];
+		return res;
+	}
+};
+const int mx = 2e5+1;
+vi adj[mx];// this is tree
+int A[mx];// weight of node
+int st[mx];//euler start
+int en[mx];//euler end
+int timer = 0;
+FT ft(mx+1);// fenwik tree
+
+void dfs(int x, int p) {
+	st[x] = timer++;
+	for(const int &e : adj[x]) if(e != p) dfs(e, x);
+	en[x] = timer-1;
+}
+
+inline void solve(){
+    int n, q;
+	cin >> n >> q;
+
+	for(int i = 1; i <= n; i++) cin >> A[i];
+
+	for(int i = 0; i < n-1; i++) {
+		int a, b; cin >> a >> b;
+		adj[a].pb(b);
+		adj[b].pb(a);
+	}
+
+	dfs(1, 0);
+
+	for(int i = 1; i <= n; i++) {
+		ft.update(st[i], A[i]);
+		ft.update(en[i]+1, -A[i]);
+	}
+
+	for(int i = 0; i < q; i++) {
+		int type, s; cin >> type >> s;
+		if(type == 1) {
+			int x; cin >> x;
+			ft.update(st[s], x-A[s]);
+			ft.update(en[s]+1, -(x-A[s])); // increment by 1
+			A[s] = x;
+		} else {
+			cout << ft.query(st[s]+1) << '\n';
+		}
+	}
+}
+signed main(){
+    IOS;
+    // prepare();
+    size_t tt = 1;
+    // cin >> tt;
+    while(tt--) solve();
+    return 0;
+}
